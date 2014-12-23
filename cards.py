d20647c53ca56f264877d7e5a307c97e55236fd6
Jonathan Gordon <jdgordy@gmail.com>
2014-12-23 19:05:03 +1100

    start parsing brown/grey cards

---
+++ cards.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+RESOURCE_WOOD = "W"
+RESOURCE_ORE = "O"
+RESOURCE_STONE = "S"
+RESOURCE_BRICK = "B"
+RESOURCE_GLASS = "G"
+RESOURCE_LOOM = "L"
+RESOURCE_PAPER = "P"
+
+SCIENCE_GEAR = "G"
+SCIENCE_COMPASS = "C"
+SCIENCE_TABLET = "T"
+
+class Card:
+	def __init__(self, name):
+		self.name = name
+		self.age = "1"
+	
+	def parse_infotext(self, text):
+		return True
+	
+	def play(self):
+		''' Called when the card is played onto the table'''
+		pass
+	
+	def score(self):
+		''' Called when the card needs to score itself end game'''
+		pass
+	
+	def get_colour(self):
+		return ""
+		
+	def get_info(self):
+		return ""
+
+	def get_name(self):
+		return self.name + " (" + self.get_colour() + ")" + " -> " + self.get_info()
+
+class BrownCard(Card):
+	valid_resources = [RESOURCE_WOOD, RESOURCE_ORE, RESOURCE_STONE, RESOURCE_BRICK]
+	def parse_infotext(self, text):
+		self.resources = []
+		self.allow_all = True
+		for r in text:
+			if r == "/":
+				self.allow_all = False
+			elif r in self.valid_resources:
+				self.resources.append(r)
+			else:
+				return False
+		return True
+	
+	def get_info(self):
+		text = ""
+		for r in self.resources:
+			if len(text) != 0:
+				if self.allow_all == False:
+					text += "/"
+			text += r
+		return text
+
+	def get_colour(self):
+		return "BROWN"
+		
+class GreyCard(BrownCard):
+	valid_resources = [RESOURCE_GLASS, RESOURCE_LOOM, RESOURCE_PAPER]
+	def get_colour(self):
+		return "GREY"
+	
+
+def build_card(colour, name, infostr):
+	card = None
+	if colour == "brown":
+		card = BrownCard(name)
+	elif colour == "grey":
+		card = GreyCard(name)
+	
+	if card != None and card.parse_infotext(infostr):
+		return card
+	
+	return None
+
+
+def read_cards_file(filename):
+	cards = []
+	with open(filename) as f:
+		content = f.readlines()
+		for line in content:
+			if line.startswith("#"):
+				continue
+			values = line.split(",")
+			if len(values) != 8:
+				continue
+			age = int(values[0].strip())
+			players = int(values[1].strip())
+			name = values[2].strip()
+			colour = values[3].strip()
+			cost = values[4].strip()
+			prebuilt = values[5].strip()
+			postbuilt = values[6].strip()
+			text = values[7].strip()
+			c = build_card(colour, name, text)
+			if c:
+				cards.append(c)
+	return cards
+			
+
+
+
+
+cards = read_cards_file("7wonders.txt")
+for c in cards:
+	print c.get_name()
